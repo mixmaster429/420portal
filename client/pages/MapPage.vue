0b5c21e459768fbd37a26e81e18eba79f02fccdf
mixmaster429 <ryancharley429@gmail.com>
2021-06-30 00:49:15 +0800

    map page bug fix

--- client/pages/MapPage.vue
+++ client/pages/MapPage.vue
@@ -1,48 +1,68 @@
 <template>
-    <map-page :page_title="map_location.page_title"></map-page>
+  <map-page :page_title="map_location.page_title"></map-page>
 </template>
 
 <script>
-    import { mapGetters } from "vuex";
-    import MapPage from "~/components/MapPage";
-    export default {
-        components: {
-            MapPage,
+import { mapGetters } from 'vuex'
+import MapPage from '~/components/MapPage'
+export default {
+  components: {
+    MapPage,
+  },
+  head() {
+    return {
+      title: `${this.map_location.city ? this.map_location.city.name + ', ' : ''}${
+        this.map_location.state ? this.map_location.state.name + ' - ' : ''
+      }Marijuana Dispensary - Marijuana Delivery`,
+      meta: [
+        {
+          hid: 'title',
+          name: 'title',
+          content: `${this.map_location.city ? this.map_location.city.name + ', ' : ''}${
+            this.map_location.state ? this.map_location.state.name + ' - ' : ''
+          }Marijuana Dispensary - Marijuana Delivery`,
         },
-        head () {
-            return {
-                title: `${this.map_location.city ? this.map_location.city.name + ', ' : ''}${this.map_location.state ? this.map_location.state.name + ' - ' : ''}Marijuana Dispensary - Marijuana Delivery`,
-                meta: [
-                    { hid: 'title', name: 'title', content: `${this.map_location.city ? this.map_location.city.name + ', ' : ''}${this.map_location.state ? this.map_location.state.name + ' - ' : ''}Marijuana Dispensary - Marijuana Delivery`},
-                    { hid: 'keywords', name: 'keywords', content: `${this.map_location.city ? this.map_location.city.name.toLowerCase() + ', ' : ''}${this.map_location.state ? this.map_location.state.name.toLowerCase() + ', ' : ''}marijuana, weed, dispensary, collective, delivery, medical, recreational, cannabis`},
-                    { hid: 'description', name: 'description', content: `Medical and Recreational Marijuana Dispensaries and Marijuana Deliveries${this.map_location.state ? ' in ' : ''}${this.map_location.city ? this.map_location.city.name + ', ' : ''}${this.map_location.state ? this.map_location.state.name : ''}. Medical and Recreational Cannabis.`}
-                ],
-            }
+        {
+          hid: 'keywords',
+          name: 'keywords',
+          content: `${this.map_location.city ? this.map_location.city.name.toLowerCase() + ', ' : ''}${
+            this.map_location.state ? this.map_location.state.name.toLowerCase() + ', ' : ''
+          }marijuana, weed, dispensary, collective, delivery, medical, recreational, cannabis`,
         },
-        watch: {
-            $route: "fetchMapLocation",
+        {
+          hid: 'description',
+          name: 'description',
+          content: `Medical and Recreational Marijuana Dispensaries and Marijuana Deliveries${
+            this.map_location.state ? ' in ' : ''
+          }${this.map_location.city ? this.map_location.city.name + ', ' : ''}${
+            this.map_location.state ? this.map_location.state.name : ''
+          }. Medical and Recreational Cannabis.`,
         },
-        serverPrefetch () {
-            return this.fetchMapLocation()
-        },
-        computed: {
-            ...mapGetters({
-                user: 'auth/user',
-                map_location: 'auth/map_location',
-            }),
-        },
-        created() {
-            
-        },
-        mounted() {
-            if (!this.map_location) {
-                this.fetchMapLocation();
-            }
-        },
-        methods: {
-            fetchMapLocation () {
-                return this.$store.dispatch('auth/fetchMapLocation', this.$route.params)
-            },
-        }
+      ],
     }
+  },
+  watch: {
+    $route: 'fetchMapLocation',
+  },
+  serverPrefetch() {
+    return this.fetchMapLocation()
+  },
+  computed: {
+    ...mapGetters({
+      user: 'auth/user',
+      map_location: 'auth/map_location',
+    }),
+  },
+  created() {},
+  mounted() {
+    if (!this.map_location) {
+      this.fetchMapLocation()
+    }
+  },
+  methods: {
+    fetchMapLocation() {
+      return this.$store.dispatch('auth/fetchMapLocation', this.$route.params)
+    },
+  },
+}
 </script>
